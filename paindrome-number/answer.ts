ca0dd91d6cc3e41a8e76df347d1ce61edb1fb5a3
nyawach <yuunyawach@gmail.com>
2022-01-03 23:46:26 +0900

    half使ってなかった

--- paindrome-number/answer.ts
+++ paindrome-number/answer.ts
@@ -3,7 +3,6 @@
 const isPalindrome = (x: number): boolean => {
     if (x < 0) return false
     if (x === 0 && x % 10 === 0) return false
-    const half = Math.floor(x / 2)
     const all = x.toString().split('').map(str => Number.parseInt(str, 10))
     const maxIndex = all.length - 1
     return all.every((n, index) => n === all[maxIndex - index])
