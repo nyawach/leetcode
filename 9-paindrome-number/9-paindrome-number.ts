af0edc7f6de65f55a8d2ebdae20c78c33fb106ca
nyawach <yuunyawach@gmail.com>
2022-01-08 13:47:30 +0900

    間違ってた

--- 9-paindrome-number/9-paindrome-number.ts
+++ 9-paindrome-number/9-paindrome-number.ts
@@ -2,7 +2,7 @@
 
 const isPalindrome = (x: number): boolean => {
     if (x < 0) return false
-    if (x === 0 && x % 10 === 0) return false
+    if (x !== 0 && x % 10 === 0) return false
     const all = x.toString().split('').map(str => Number.parseInt(str, 10))
     const maxIndex = all.length - 1
     return all.every((n, index) => n === all[maxIndex - index])
