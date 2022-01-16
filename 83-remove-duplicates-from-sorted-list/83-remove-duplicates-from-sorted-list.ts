a635728ecc37f9c2e25e227bf3b5f0e03f6e7495
nyawach <yuunyawach@gmail.com>
2022-01-16 20:51:36 +0900

    remove dup from sorted list
    ref: https://leetcode.com/problems/remove-duplicates-from-sorted-list/discuss/28722/Javascript-Solution

---
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.ts
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=83 lang=typescript
+ *
+ * [83] Remove Duplicates from Sorted List
+ */
+
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val==null ? 0 : val)
+        this.next = (next==null ? null : next)
+    }
+}
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+// [1,2,3,4,5,5,6,7,7,7,7,7,9,10,11]
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let current = head
+    while (current && current.next) {
+        if (current.val === current.next.val) {
+            current.next = current.next.next
+        } else {
+            current = current.next
+        }
+    }
+    return head
+}
+// @lc code=end
