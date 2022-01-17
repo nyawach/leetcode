c30a17866928c17719f4fbac079a1ba1a19a2ec4
nyawach <yuunyawach@gmail.com>
2022-01-17 21:51:18 +0900

    merge sorted array
    正攻法ではなさそうなかんじがする

--- 88-merge-sorted-array/88-merge-sorted-array.ts
+++ 88-merge-sorted-array/88-merge-sorted-array.ts
@@ -8,6 +8,8 @@
 // [1]\n1\n[]\n0
 // [0]\n0\n[1]\n1
 // [1,2,3,9,9,0,0,0]\n5\n[2,4,6]\n3
+// [-1,0,0,3,3,3,0,0,0]\n6\n[1,2,2]\n3
+// [1,2,3,0,0,0]\n3\n[2,5,6]\n3
 
 // @lc code=start
 /**
@@ -16,31 +18,28 @@
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
     let i1 = 0
     let i2 = 0
-    let len = m + n
-    let i = 0
-    let temp = 0
-    while(i < len) {
-        const n1 = temp ? temp : nums1[i1] ?? 0
-        const n2 = nums2[i2] ?? 0
-        if (!n1) {
-            nums1[i1] = n2
-            i1++
-            i2++
-        } else if (!n2) {
-            nums1[i1] = n1
-            i1++
-        } else if (n1 <= n2) {
-            temp = 0
-            nums1[i1] = n1
+    const result: number[] = []
+    while (i1 < m && i2 < n) {
+        const n1 = nums1[i1]
+        const n2 = nums2[i2]
+        if (n1 <= n2) {
+            result.push(n1)
             i1++
         } else {
-            temp = n1
-            nums1[i1] = n2
-            i1++
+            result.push(n2)
             i2++
         }
-        i++
     }
+    while (i1 < m) {
+        const n1 = nums1[i1]
+        result.push(n1)
+        i1++
+    }
+    while (i2 < n) {
+        const n2 = nums2[i2]
+        result.push(n2)
+        i2++
+    }
+    nums1.splice(0, m+n, ...result)
 };
 // @lc code=end
-
