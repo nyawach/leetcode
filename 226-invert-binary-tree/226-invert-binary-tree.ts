4e2719320614432b653a9591617648d30aa8cef8
nyawach <yuunyawach@gmail.com>
2022-02-13 13:52:51 +0900

    226
    - 再帰いい感じにとりいれれた
    - 時間測ってないけど5分以内に負えれた気がする

---
+++ 226-invert-binary-tree/226-invert-binary-tree.ts
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=226 lang=typescript
+ *
+ * [226] Invert Binary Tree
+ */
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+    if (root == null) return null
+    if (!root.left && !root.right) return root
+    const temp = root.right
+    root.right = invertTree(root.left)
+    root.left = invertTree(temp)
+    return root
+}
+// @lc code=end
