287d6d5263ee53a5eaadf2c86b446cff1a64534e
nyawach <yuunyawach@gmail.com>
2022-02-23 21:46:27 +0900

    292
    再帰的に考えたら、4の倍数じゃなければOKでは？となった

---
+++ 292-nim-game/292-nim-game.ts
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=292 lang=typescript
+ *
+ * [292] Nim Game
+ */
+
+// 239092130
+// 239092132
+
+// @lc code=start
+function canWinNim(n: number): boolean {
+    return n % 4 !== 0
+}
+// @lc code=end
